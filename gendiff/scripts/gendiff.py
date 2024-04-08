07e8653ada3c01a28093530050cbd967b4a7f40f
mishablokhin <mishablokhin@yandex.ru>
2024-04-08 23:49:24 +0300

    Fixed typo in help text

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', help='path to first file')
     parser.add_argument('second_file', help='path to second file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output.')
     
     args = parser.parse_args()
     
