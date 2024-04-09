fdab1480ac5b05e8930811fb2a408deaee3cd195
mishablokhin <mishablokhin@yandex.ru>
2024-04-09 19:29:00 +0300

    Refactor code to use function to read JSON files

--- gendiff/modules/gendiff.py
+++ gendiff/modules/gendiff.py
@@ -1,16 +1,19 @@
 import json
 
-
 def format_value(value):
     if isinstance(value, bool):
         return str(value).lower()
     return value
 
 
+def read_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
 def generate_diff(file_path1, file_path2):
-    with open(file_path1, 'r') as file1, open(file_path2, 'r') as file2:
-        dict1 = json.load(file1)
-        dict2 = json.load(file2)
+    dict1 = read_json_file(file_path1)
+    dict2 = read_json_file(file_path2)
 
     all_dict_keys = sorted(set(dict1) | set(dict2))
     diffs = []
@@ -24,5 +27,5 @@ def generate_diff(file_path1, file_path2):
             diffs.append(f"  + {key}: {format_value(dict2[key])}")
         else:
             diffs.append(f"    {key}: {format_value(dict1[key])}")
-
+    
     return '{\n' + '\n'.join(diffs) + '\n}'
