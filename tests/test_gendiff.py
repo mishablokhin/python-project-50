be4959ab33da7e2c9b677a313d9125b255e4e3e7
mishablokhin <mishablokhin@yandex.ru>
2024-04-14 16:28:00 +0300

    Added test for compare two .yaml files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,14 +2,24 @@ import pytest
 from gendiff.modules.gendiff import generate_diff
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     path1 = 'tests/fixtures/file1.json'
     path2 = 'tests/fixtures/file2.json'
     
-    # Путь к файлу с ожидаемым результатом
     expected_result_path = 'tests/fixtures/expected_diff.txt'
     
-    # Считывание ожидаемого результата из файла
+    with open(expected_result_path) as f:
+        expected_result = f.read().strip()
+    
+    assert generate_diff(path1, path2) == expected_result
+
+
+def test_generate_diff_yaml():
+    path1 = 'tests/fixtures/file1.yaml'
+    path2 = 'tests/fixtures/file2.yaml'
+    
+    expected_result_path = 'tests/fixtures/expected_diff.txt'
+    
     with open(expected_result_path) as f:
         expected_result = f.read().strip()
     
