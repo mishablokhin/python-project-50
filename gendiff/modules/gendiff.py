47cbe3e6323ed3d5dac9f37b1dc062fe8f7663bb
mishablokhin <mishablokhin@yandex.ru>
2024-04-21 12:24:18 +0300

    Rewrite code to build diff and return it to another formatter module

--- gendiff/modules/gendiff.py
+++ gendiff/modules/gendiff.py
@@ -1,27 +1,35 @@
 from gendiff.modules.parse_file import read_data_file
+from gendiff.modules.formatters import format_diff
 
 
-def format_value(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    return value
+def build_diff(data1, data2):
+    keys = sorted(set(data1.keys()) | set(data2.keys()))
+    diff = {}
+    for key in keys:
+        if key in data1 and key in data2:
+            if isinstance(data1[key], dict) and isinstance(data2[key], dict):
+                child_diff = build_diff(data1[key], data2[key])
+                diff[key] = {
+                    'type': 'nested',
+                    'children': child_diff
+                }
+            elif data1[key] != data2[key]:
+                diff[key] = {
+                    'type': 'changed',
+                    'old_value': data1[key],
+                    'new_value': data2[key]
+                }
+            else:
+                diff[key] = {'type': 'unchanged', 'value': data1[key]}
+        elif key in data1:
+            diff[key] = {'type': 'removed', 'value': data1[key]}
+        else:
+            diff[key] = {'type': 'added', 'value': data2[key]}
+    return diff
 
 
 def generate_diff(file_path1, file_path2):
     dict1 = read_data_file(file_path1)
     dict2 = read_data_file(file_path2)
-
-    all_dict_keys = sorted(set(dict1) | set(dict2))
-    diffs = []
-    for key in all_dict_keys:
-        if key in dict1 and key not in dict2:
-            diffs.append(f"  - {key}: {format_value(dict1[key])}")
-        elif key not in dict1 and key in dict2:
-            diffs.append(f"  + {key}: {format_value(dict2[key])}")
-        elif dict1[key] != dict2[key]:
-            diffs.append(f"  - {key}: {format_value(dict1[key])}")
-            diffs.append(f"  + {key}: {format_value(dict2[key])}")
-        else:
-            diffs.append(f"    {key}: {format_value(dict1[key])}")
-
-    return '{\n' + '\n'.join(diffs) + '\n}'
+    diff = build_diff(dict1, dict2)
+    return format_diff(diff)
