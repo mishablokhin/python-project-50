4eb0bed108666c8e311c3a2d9891880ccd4285a1
mishablokhin <mishablokhin@yandex.ru>
2024-04-21 12:23:02 +0300

    Added 2 new tests to chech result of making diff of nested files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -24,3 +24,27 @@ def test_generate_diff_yaml():
         expected_result = f.read().strip()
     
     assert generate_diff(path1, path2) == expected_result
+
+
+def test_generate_diff_nested_yaml():
+    path1 = 'tests/fixtures/file1_nested.yaml'
+    path2 = 'tests/fixtures/file2_nested.yaml'
+    
+    expected_result_path = 'tests/fixtures/expected_diff_nested.txt'
+    
+    with open(expected_result_path) as f:
+        expected_result = f.read().strip()
+    
+    assert generate_diff(path1, path2) == expected_result
+
+
+def test_generate_diff_nested_json():
+    path1 = 'tests/fixtures/file1_nested.json'
+    path2 = 'tests/fixtures/file2_nested.json'
+    
+    expected_result_path = 'tests/fixtures/expected_diff_nested.txt'
+    
+    with open(expected_result_path) as f:
+        expected_result = f.read().strip()
+    
+    assert generate_diff(path1, path2) == expected_result
