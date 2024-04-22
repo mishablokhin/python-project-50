79dbc2ed1181173bb1708d638bfa0839556659c3
mishablokhin <mishablokhin@yandex.ru>
2024-04-22 19:20:24 +0300

    Added cli.py function import

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python3
-import argparse
-from gendiff.modules.gendiff import generate_diff
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', help='path to first file')
-    parser.add_argument('second_file', help='path to second file')
-    parser.add_argument('-f', '--format', choices=['stylish', 'plain', 'json'],
-                        default='stylish', help='set format of output')
-
-    args = parser.parse_args()
-
-    diff = generate_diff(args.first_file, args.second_file, args.format)
-    print(diff)
+from gendiff.cli import main
 
 
 if __name__ == '__main__':
