deb4af48cab8abe21158765e73f7075e93005a2e
mishablokhin <mishablokhin@yandex.ru>
2024-04-22 19:16:34 +0300

    Rewrited tests to use parametrize decorator

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,73 +1,18 @@
-from gendiff.modules.gendiff import generate_diff
+import pytest
+from gendiff import generate_diff
 
 
-def test_generate_diff_json():
-    path1 = 'tests/fixtures/file1.json'
-    path2 = 'tests/fixtures/file2.json'
+test_data = [
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/expected_diff.txt', None),
+    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/expected_diff.txt', None),
+    ('tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml', 'tests/fixtures/expected_diff_nested.txt', None),
+    ('tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json', 'tests/fixtures/expected_diff_nested.txt', None),
+    ('tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml', 'tests/fixtures/expected_diff_nested_plain.txt', 'plain'),
+    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/expected_diff_json.txt', 'json')
+]
 
-    expected_result_path = 'tests/fixtures/expected_diff.txt'
-
-    with open(expected_result_path) as f:
+@pytest.mark.parametrize("file1, file2, expected_file, format_name", test_data)
+def test_generate_diff(file1, file2, expected_file, format_name):
+    with open(expected_file) as f:
         expected_result = f.read().strip()
-
-    assert generate_diff(path1, path2) == expected_result
-
-
-def test_generate_diff_yaml():
-    path1 = 'tests/fixtures/file1.yaml'
-    path2 = 'tests/fixtures/file2.yaml'
-
-    expected_result_path = 'tests/fixtures/expected_diff.txt'
-
-    with open(expected_result_path) as f:
-        expected_result = f.read().strip()
-
-    assert generate_diff(path1, path2) == expected_result
-
-
-def test_generate_diff_nested_yaml():
-    path1 = 'tests/fixtures/file1_nested.yaml'
-    path2 = 'tests/fixtures/file2_nested.yaml'
-
-    expected_result_path = 'tests/fixtures/expected_diff_nested.txt'
-
-    with open(expected_result_path) as f:
-        expected_result = f.read().strip()
-
-    assert generate_diff(path1, path2) == expected_result
-
-
-def test_generate_diff_nested_json():
-    path1 = 'tests/fixtures/file1_nested.json'
-    path2 = 'tests/fixtures/file2_nested.json'
-
-    expected_result_path = 'tests/fixtures/expected_diff_nested.txt'
-
-    with open(expected_result_path) as f:
-        expected_result = f.read().strip()
-
-    assert generate_diff(path1, path2) == expected_result
-
-
-def test_generate_diff_nested_yaml_plain():
-    path1 = 'tests/fixtures/file1_nested.yaml'
-    path2 = 'tests/fixtures/file2_nested.yaml'
-
-    expected_result_path = 'tests/fixtures/expected_diff_nested_plain.txt'
-
-    with open(expected_result_path) as f:
-        expected_result = f.read().strip()
-
-    assert generate_diff(path1, path2, 'plain') == expected_result
-
-
-def test_generate_diff_json_format():
-    path1 = 'tests/fixtures/file1.yaml'
-    path2 = 'tests/fixtures/file2.yaml'
-
-    expected_result_path = 'tests/fixtures/expected_diff_json.txt'
-
-    with open(expected_result_path) as f:
-        expected_result = f.read().strip()
-
-    assert generate_diff(path1, path2, 'json') == expected_result
+    assert generate_diff(file1, file2, format_name) == expected_result
