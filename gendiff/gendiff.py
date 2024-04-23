797149dace703073df52c29092d93f62430d71b4
mishablokhin <mishablokhin@yandex.ru>
2024-04-23 20:11:32 +0300

    Fixed imports and generate_diff function to work with new format_diff function from __init__.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,5 +1,5 @@
-from gendiff.parse_file import read_data_file
-from gendiff.formatters import get_formatter
+from gendiff.parse_file import get_content
+from gendiff.formatters import format_diff
 
 
 def build_diff(data1, data2):
@@ -41,8 +41,7 @@ def handle_changed_value(data1, data2, key):
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
-    dict1 = read_data_file(file_path1)
-    dict2 = read_data_file(file_path2)
+    dict1 = get_content(file_path1)
+    dict2 = get_content(file_path2)
     diff = build_diff(dict1, dict2)
-    formatter = get_formatter(format_name)
-    return formatter(diff)
+    return format_diff(diff, format_name)
