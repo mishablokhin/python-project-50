fb4531e018a797769065f42bdabceff814b3b903
mishablokhin <mishablokhin@yandex.ru>
2024-04-23 20:02:21 +0300

    Added function which returns filepath to reduce amount of code with fixtures data

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,18 +2,22 @@ import pytest
 from gendiff import generate_diff
 
 
+def file_path(filename):
+    return f"tests/fixtures/{filename}"
+
+
 test_data = [
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/expected_diff.txt', None),
-    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/expected_diff.txt', None),
-    ('tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml', 'tests/fixtures/expected_diff_nested.txt', None),
-    ('tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json', 'tests/fixtures/expected_diff_nested.txt', None),
-    ('tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml', 'tests/fixtures/expected_diff_nested_plain.txt', 'plain'),
-    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/expected_diff_json.txt', 'json')
+    (file_path('file1.json'), file_path('file2.json'), file_path('expected_diff.txt'), None),
+    (file_path('file1.yaml'), file_path('file2.yaml'), file_path('expected_diff.txt'), None),
+    (file_path('file1_nested.yaml'), file_path('file2_nested.yaml'), file_path('expected_diff_nested.txt'), None),
+    (file_path('file1_nested.json'), file_path('file2_nested.json'), file_path('expected_diff_nested.txt'), None),
+    (file_path('file1_nested.yaml'), file_path('file2_nested.yaml'), file_path('expected_diff_nested_plain.txt'), 'plain'),
+    (file_path('file1.yaml'), file_path('file2.yaml'), file_path('expected_diff_json.txt'), 'json')
 ]
 
 
 @pytest.mark.parametrize("file1, file2, expected_file, format_name", test_data)
 def test_generate_diff(file1, file2, expected_file, format_name):
     with open(expected_file) as f:
-        expected_result = f.read().strip()
-    assert generate_diff(file1, file2, format_name) == expected_result
+        expected_result = f.read()
+        assert generate_diff(file1, file2, format_name) == expected_result
