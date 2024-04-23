616b84d98f541e0124f7cac1c6fcbc97b0b81cd9
mishablokhin <mishablokhin@yandex.ru>
2024-04-23 20:10:17 +0300

    Reworked function format_diff to get content and formatter name to return result

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,9 +1,11 @@
-from . import plain, json, stylish
+from .stylish import stylish
+from .plain import plain
+from .json import json_format
 
 
-def get_formatter(format_name):
+def format_diff(diff, format_name='stylish'):
     if format_name == 'plain':
-        return plain.plain
+        return plain(diff)
     elif format_name == 'json':
-        return json.json_format
-    return stylish.stylish
+        return json_format(diff)
+    return stylish(diff)
