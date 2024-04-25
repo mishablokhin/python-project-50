2520cd14b3ca00e2aec4f04cbe51efb06c8ff9e4
mishablokhin <mishablokhin@yandex.ru>
2024-04-25 14:42:29 +0300

    Rewrite file fith os.path and make some small changes

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,23 +1,27 @@
 import pytest
+import os
 from gendiff import generate_diff
 
 
-def file_path(filename):
-    return f"tests/fixtures/{filename}"
+def get_fixture_path(filename):
+    return os.path.join('tests', 'fixtures', filename)
 
 
 test_data = [
-    (file_path('file1.json'), file_path('file2.json'), file_path('expected_diff.txt'), None),
-    (file_path('file1.yaml'), file_path('file2.yaml'), file_path('expected_diff.txt'), None),
-    (file_path('file1_nested.yaml'), file_path('file2_nested.yaml'), file_path('expected_diff_nested.txt'), None),
-    (file_path('file1_nested.json'), file_path('file2_nested.json'), file_path('expected_diff_nested.txt'), None),
-    (file_path('file1_nested.yaml'), file_path('file2_nested.yaml'), file_path('expected_diff_nested_plain.txt'), 'plain'),
-    (file_path('file1.yaml'), file_path('file2.yaml'), file_path('expected_diff_json.txt'), 'json')
+    ('file1.json', 'file2.json', 'expected_diff.txt', None),
+    ('file1.yaml', 'file2.yaml', 'expected_diff.txt', None),
+    ('file1_nested.yaml', 'file2_nested.yaml', 'expected_diff_nested.txt', None),
+    ('file1_nested.json', 'file2_nested.json', 'expected_diff_nested.txt', None),
+    ('file1_nested.yaml', 'file2_nested.yaml', 'expected_diff_nested_plain.txt', 'plain'),
+    ('file1.yaml', 'file2.yaml', 'expected_diff_json.txt', 'json')
 ]
 
 
 @pytest.mark.parametrize("file1, file2, expected_file, format_name", test_data)
 def test_generate_diff(file1, file2, expected_file, format_name):
+    file1 = get_fixture_path(file1)
+    file2 = get_fixture_path(file2)
+    expected_file = get_fixture_path(expected_file)
     with open(expected_file) as f:
         expected_result = f.read()
-        assert generate_diff(file1, file2, format_name) == expected_result
+    assert generate_diff(file1, file2, format_name) == expected_result
